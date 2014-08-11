c4846cbd4ffe9fc6bb8401a3f3e7b00511c7232e
Christopher Torgalson <manager@bedlamhotel.com>
2014-08-11 13:10:58 +0200

    Clean up pre-coffee nonsense

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer       'Christopher Torgalson'
 maintainer_email 'manager@bedlamhotel.com'
 license          'Apache 2.0'
 description      'Installs drush'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports debian
-supports ubuntu
+supports "debian"
+supports "ubuntu"
 
-depends apt
+depends "apt"
+depends "php"
